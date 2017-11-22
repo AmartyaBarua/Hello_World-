3be22bd044e753e01d08a03ed3afd4f1358c3522
Amar <Amar@amartyas-mbp.home>
2017-11-22 15:44:33 -0500

    Initial Commit

---
+++ Hello_World!/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello_World!
+//
+//  Created by Amar on 11/22/17.
+//  Copyright © 2017 Amar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
